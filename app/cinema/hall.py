7581d0d67db890c7d86d6c5680308742f9b0f04a
OldiLo <ostapbataj79@gmail.com>
2026-05-29 00:49:14 +0300

    fixes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -3,14 +3,11 @@ from app.people.customer import Customer
 
 
 class CinemaHall:
-    def __init__(self, number: int = None, hall_number: int = None) -> None:
-        self.number = number if number is not None else hall_number
+    def __init__(self, number: int = None) -> None:
+        self.number = number
 
     def movie_session(
-        self,
-        movie_name: str,
-        customers: list[Customer],
-        cleaning_staff: Cleaner
+        self, movie_name: str, customers: list[Customer], cleaning_staff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
