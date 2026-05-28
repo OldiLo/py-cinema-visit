102671d106f7d2f44d2f53e77a58ae3cf7a601e0
OldiLo <ostapbataj79@gmail.com>
2026-05-29 00:50:27 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+    customers: list,
+    hall_number: int,
+    cleaner: str,
+    movie: str
+) -> None:
     customers_class = [Customer(cus["name"], cus["food"]) for cus in customers]
     for customer in customers_class:
         CinemaBar.sell_product(customer, customer.food)
